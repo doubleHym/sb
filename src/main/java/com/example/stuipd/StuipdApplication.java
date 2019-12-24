17233036c7d4324a73bb2b05ab24e3d4395fadc7
heym <2287977034@qq.com>
2019-12-24 14:54:28 +0800

    origin

---
+++ src/main/java/com/example/stuipd/StuipdApplication.java
@@ -0,0 +1,13 @@
+package com.example.stuipd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuipdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuipdApplication.class, args);
+    }
+
+}
